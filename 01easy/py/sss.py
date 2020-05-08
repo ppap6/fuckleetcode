a1b1ab2382fddbd4cff2716237f81f37e90ed789
ainy15 <way787217849@126.com>
2020-05-08 14:26:43 +0800

    [add] medium/221.py

---
+++ 01easy/py/sss.py
@@ -0,0 +1,9 @@
+das = {'gender': ['1'],
+          'user_name': ['甲基绿'],
+          'email': ['787217848@qq.com'],
+          'role_id': ['2']
+          }
+for i, v in das.items():
+    das[i] = v[0]
+print(das)
+print([0] * 5)
