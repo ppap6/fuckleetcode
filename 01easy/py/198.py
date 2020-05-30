c3b23d759cec2a980cfec49f734e364944fef05e
ainy15 <way787217849@126.com>
2020-05-30 11:42:16 +0800

    [refactor]easy/198.py

--- 01easy/py/198.py
+++ 01easy/py/198.py
@@ -7,3 +7,17 @@
 一夜之内能够偷窃到的最高金额。
 
 '''
+# 归类：动态规划
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0: return 0
+        if n == 1: return nums[0]
+        dp = [0] * n
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[-1]
