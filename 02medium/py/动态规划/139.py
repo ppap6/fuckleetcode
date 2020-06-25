acc889e824c52d05bec7e1063f488d4de7a5f886
ainy15 <way787217849@126.com>
2020-06-25 20:47:14 +0800

    [feat] medium/139.py

---
+++ 02medium/py/动态规划/139.py
@@ -0,0 +1,19 @@
+'''
+给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，
+判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+示例 1：
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False]*(n+1)
+        dp[0] = True
+        for i in range(n):
+            if not dp[i]: continue
+            for j in range(i+1, n+1):
+                if dp[i] and (s[i:j] in wordDict):
+                    dp[j] = True
+        return dp[-1]
